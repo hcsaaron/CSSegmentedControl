ad62824048d14b0faccec65fdfa5dc5398e35fcc
Aaron <hcsaaron@163.com>
2018-11-21 01:33:51 +0800

    new files

---
+++ CSSegmentedControl/CSSegmentedControl/CSSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  CSSegmentedControl.h
+//  CSSegmentedControl
+//
+//  Created by 黄楚升 on 2018/11/20.
+//  Copyright © 2018年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSSegmentedControl.
+FOUNDATION_EXPORT double CSSegmentedControlVersionNumber;
+
+//! Project version string for CSSegmentedControl.
+FOUNDATION_EXPORT const unsigned char CSSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSegmentedControl/PublicHeader.h>
+
+
---
+++ CSSegmentedControl/CSSegmentedControlTests/CSSegmentedControlTests.swift
@@ -0,0 +1,34 @@
+//
+//  CSSegmentedControlTests.swift
+//  CSSegmentedControlTests
+//
+//  Created by 黄楚升 on 2018/11/20.
+//  Copyright © 2018年 Aaron. All rights reserved.
+//
+
+import XCTest
+@testable import CSSegmentedControl
+
+class CSSegmentedControlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
